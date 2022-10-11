e90f60433dce9663b2d5ccc18da3542b22dcee14
Oliver Townell <o.townell@EDU.epsb.ca>
2022-10-11 13:37:30 -0600

    m

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // JavaScript Code
+//input
+// let username = prompt("What is your name")
+// let timeOfDay = prompt("what time of day is it")
+//process
+let msg = `hello ${username}. have a great ${timeOfDay}!`
+//output
+// alert(msg);
